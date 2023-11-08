46c49375f1ee9892186da784e7bc79fbe389258c
Dorian <dorianoriginaldesings@gmail.com>
2023-11-08 12:14:41 +0100

    update

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,34 +1,13 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { createAccessToken } from '../../utils/jsw.js';
+import { createAccessToken } from '../../utils/jwt.js';
 import UserModel from '../models/user.model.js';
 const TOKEN_SECRET = 'secret';
-
-const register = async (req, res) => {
-  const { username, email, password } = req.body;
-
-  try {
-    // Generar un hash de la contraseña
-    const saltRounds = 10; // Número de rondas de sal para la encriptación
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    const newUser = new UserModel({
-      username,
-      email,
-      password: hashedPassword // Guarda la contraseña encriptada en la base de datos
-    });
-
-    await newUser.save();
-    res.send({ message: 'register' });
-  } catch (error) {
-    console.error('Error al registrar al usuario:', error);
-    res.status(500).send({ error: 'Error al registrar al usuario' });
-  }
-};
 
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
+
     const userFound = await UserModel.findOne({ email });
 
     if (!userFound)
@@ -58,6 +37,28 @@ const login = async (req, res) => {
     });
   } catch (error) {
     return res.status(500).json({ message: error.message });
+  }
+};
+
+const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    // Generar un hash de la contraseña
+    const saltRounds = 10; // Número de rondas de sal para la encriptación
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const newUser = new UserModel({
+      username,
+      email,
+      password: hashedPassword // Guarda la contraseña encriptada en la base de datos
+    });
+
+    await newUser.save();
+    res.send({ message: 'register' });
+  } catch (error) {
+    console.error('Error al registrar al usuario:', error);
+    res.status(500).send({ error: 'Error al registrar al usuario' });
   }
 };
 
--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -3,8 +3,8 @@ import { login, register, verifyToken } from '../controllers/auth.controller.js'
 
 const authRoutes = Router();
 
-authRoutes.post('/auth/register', register);
 authRoutes.post('/auth/login', login);
+authRoutes.post('/auth/register', register);
 authRoutes.get('/auth/verifyToken', verifyToken);
 
 export default authRoutes;
